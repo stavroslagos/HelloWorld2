a7c26355686eb2f1d96f3057cb637341232b1861
Stavros Lagos <stavroslagos0@gmail.com>
2019-02-25 12:34:43 +0200

    info added test

--- app/src/main/java/helloworld/rolen4lab/lan/MainActivity.java
+++ app/src/main/java/helloworld/rolen4lab/lan/MainActivity.java
@@ -37,6 +37,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
+// test info
+
 public class MainActivity extends AppCompatActivity {
     private TextView mString2Byte;
     private EditText mPlainString;
